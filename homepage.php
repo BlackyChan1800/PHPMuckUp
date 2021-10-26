9ced55938fea6d605fe9f4592488b7d7786733df
BlackyChan1800 <76032223+BlackyChan1800@users.noreply.github.com>
2021-10-26 18:26:22 +0800

    fix links tab be inline and connect all tabs

--- homepage.php
+++ homepage.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test Site</title>
-    <link rel="stylesheet" href="../css/contactus.css" type="text/css">
+    <link rel="stylesheet" href="homepage.css" type="text/css">
 </head>
 
 <body>
@@ -15,19 +15,19 @@
     <header>
         <ul class="left-nav">
             <li>
-                <a href="../homepage.php"><img src="../images/logo.gif" height="50px"></a>
+                <a href="homepage.php"><img src="images/logo.gif" height="50px"></a>
             </li>
         </ul>
         <ul class="right-nav">
             <ul class="links">
                 <li>
                     <a href="https://github.com/BlackyChan1800/PHPMuckUp">
-                    <img src="../images/pngegg.png" height="25px"> </a>
+                    <img src="images/pngegg.png" height="25px" > </a>
                 </li>
             </ul>
-            <li><a href="../homepage.php"> <?php echo "Home" ?> </a></li>
-            <li><a href="aboutus.php"> <?php echo "About Us" ?> </a></li>
-            <li><a href="contactus.php"> <?php echo "Contact Us" ?> </a></li>
+            <li><a href="homepage.php"> <?php echo "Home" ?> </a></li>
+            <li><a href="sub pages/aboutus.php"> <?php echo "About Us" ?> </a></li>
+            <li><a href="sub pages/contactus.php"> <?php echo "Contact Us" ?> </a></li>
         </ul>
     </header>
     </section>
--- sub pages/aboutus.php
+++ sub pages/aboutus.php
@@ -5,10 +5,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
+    <title>Test Site</title>
+    <link rel="stylesheet" href="../css/aboutus.css" type="text/css">
 </head>
 
 <body>
+    
+    <section>
+    <header>
+        <ul class="left-nav">
+            <li>
+                <a href="../homepage.php"><img src="../images/logo.gif" height="50px"></a>
+            </li>
+        </ul>
+        <ul class="right-nav">
+            <ul class="links">
+                <li>
+                    <a href="https://github.com/BlackyChan1800/PHPMuckUp">
+                    <img src="../images/pngegg.png" height="25px"> </a>
+                </li>
+            </ul>
+            <li><a href="../homepage.php"> <?php echo "Home" ?> </a></li>
+            <li><a href="aboutus.php"> <?php echo "About Us" ?> </a></li>
+            <li><a href="contactus.php"> <?php echo "Contact Us" ?> </a></li>
+        </ul>
+    </header>
+    <div>
+        <?php echo "About Us page Here." ?>
+    </div>
+    </section>
 
 </body>
 
