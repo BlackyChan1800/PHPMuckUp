c976ace4e798fd622430d0ed769b167e4328b38c
BlackyChan1800 <76032223+BlackyChan1800@users.noreply.github.com>
2021-10-27 21:31:28 +0800

    fix links and center button

--- homepage.js
+++ homepage.js
@@ -1,29 +1,27 @@
-//Background Blur with Percentage Text
-const loadText = document.querySelector('.loading-text')
-const section1 = document.querySelector('.section1')
+const loadText = document.querySelector('.loading-text');
+const section1 = document.querySelector('.section1');
+const loadButton = document.querySelector('.container');
 
+//Create a scale function for our Opacity between 2 objects
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
 
-let load = 0
+let load = 0;
 
-let int = setInterval(blurring, 30)
+let int = setInterval(blurring, 30);
 
 function blurring() {
     load++
 
-    if (load > 99) {
-        clearInterval(int)
+    if (load > 150) {
+        clearInterval(int);
     }
 
     loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    //Sharingan Button Opacity due to Pecentage Text Increase
+    loadButton.style.opacity = scale(load, 100, 150, 0, 1);
 
     section1.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
-
-//Sharingan Button Opacity due to Pecentage Text Increase
-// const loadButton = document.querySelector('.center')
-
-// loadButton.style.opacity = loadText.style.opacity
